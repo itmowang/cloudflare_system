b7c9c53f221577c09ce2b22fe0739f60b9d713f4
itmowang <137391282+itmowang@users.noreply.github.com>
2024-09-01 22:29:18 +0800

    test
    test

--- packages/workers/index.ts
+++ packages/workers/index.ts
@@ -1,9 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
+
+export interface Env {
+  DB: D1Database;
+}
+
 export default {
-	fetch() {
-		return new Response('Hello Mowang!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
+  async fetch(request: Request, env: Env, ctx: any): Promise<Response> {
+    const adapter = new PrismaD1(env.DB);
+
+    const prisma = new PrismaClient({ adapter });
+
+    const users = await prisma.user.findMany();
+
+    const result = JSON.stringify(users);
+    return new Response(result);
+  },
 };
